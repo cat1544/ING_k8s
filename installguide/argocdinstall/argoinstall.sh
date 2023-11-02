9db5cd77df3c4097f3799a6eb27971687a36f12b
cat1544 <dbstl159@naver.com>
2023-11-02 08:34:23 +0000

    argocd ingress 완

--- installguide/argocdinstall/argoinstall.sh
+++ installguide/argocdinstall/argoinstall.sh
@@ -14,79 +14,30 @@
 # helm repo update
 # echo "Helm chart added."
 
-# Create values.yaml
-cat <<EOF > values.yaml
-apiVersionOverrides:
-  cloudgoogle: "cloud.google.com/v1"
-
-global:
-  nodeSelector:
-    cloud.google.com/gke-nodepool: argocd
-
-# server:
-#   metrics:
-#     service:
-#       annotations:
-#         cloud.google.com/neg: '{"ingress": true}'
-#         cloud.google.com/load-balancer-neg-ready: true
-#   ingress:
-#     enabled: true
-#     annotations: 
-#       kubernetes.io/ingress.global-static-ip-name: argocd-ip
-#     hosts:
-#       - www.dev-boutique.shop
-#     paths:
-#       - /
-#     pathType: Prefix
-
-# gpt
-server:
-  ingress:
-    enabled: true
-    annotations:
-      kubernetes.io/ingress.global-static-ip-name: "argocd-ip"
-      # kubernetes.io/ingress.allow-http: "false"
-      # networking.gke.io/managed-certificates: "argocd-ssl-policy"
-      kubernetes.io/ingress.class: "gce"
-      beta.cloud.google.com/backend-config: '{"ports": {"http":"argocd-backendconfig"}}'
-    hosts:
-      - www.dev-boutique.shop
-    # tls:
-    #   - secretName: argocd-ssl
-    #     hosts:
-    #       - argocd.example.com
-
-  # GKEbackendConfig:
-  #   enabled: true
-  #   spec:
-  #     securityPolicy:
-  #       name: argocd-security-policy
-  #     logging:
-  #       enable: true
-  #       sampleRate: 0.5
-  #     sessionAffinity:
-  #       affinityType: "GENERATED_COOKIE"
-  #       affinityCookieTtlSec: 50
-
-  # GKEmanagedCertificate:
-  #   enabled: ture
-  #   domains:
-  #   - www.dev-boutique.shop
-  #   - dev-boutique.shop
-
-  # GKEfrontendConfig:
-  #   enabled: true
-  #   spec:
-  #     sslPolicy: argocd-ssl-policy
-  #     redirectToHttps:
-  #       enabled: true
-  #       responseCodeName: ""
-EOF
-
 # Install argocd
 echo "Installing ArgoCD..."
-helm install argocd -f values.yaml argo/argo-cd --namespace argocd --create-namespace 
+kubectl apply -n argocd -f ~/ING_k8s/installguide/argocdyaml/argocd.yaml
 echo "ArgoCD installation complete."
+
+# ManagedCertificate가 Active 상태가 될 때까지 확인
+CERT_NAME="argocd-certificate" # ManagedCertificate 이름
+NAMESPACE="argocd" # 네임스페이스
+
+echo "ManagedCertificate의 상태를 확인합니다."
+
+while true; do
+    # ManagedCertificate의 상태를 가져옵니다.
+    STATUS=$(kubectl get managedcertificate $CERT_NAME -n $NAMESPACE -o jsonpath='{.status.certificateStatus}')
+    
+    # 상태가 Active인지 확인합니다.
+    if [ "$STATUS" == "Active" ]; then
+        echo "ManagedCertificate가 활성화되었습니다. 다음 단계로 진행합니다."
+        break
+    else
+        echo "ManagedCertificate가 아직 활성화되지 않았습니다. 10초 후에 다시 시도합니다."
+        sleep 10
+    fi
+done
 
 # values.yaml 파일 재생성 (argo-rollouts 용)
 cat <<EOF > values.yaml
@@ -119,54 +70,53 @@ sudo mv ./kubectl-argo-rollouts-linux-amd64 /usr/local/bin/kubectl-argo-rollouts
 echo "Changing ArgoCD's service type to LoadBalancer..."
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-# Wait for External IP
-echo "Waiting for ArgoCD External IP..."
-while true; do
-    ARGOCD_SERVER=$(kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-    if [[ ! -z "$ARGOCD_SERVER" ]]; then
-        break
-    fi
-    sleep 10
-done
-echo "ArgoCD External IP: $ARGOCD_SERVER"
+# # Wait for External IP
+# echo "Waiting for ArgoCD External IP..."
+# while true; do
+#     ARGOCD_SERVER=$(kubectl get svc argocd-server -n argocd -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+#     if [[ ! -z "$ARGOCD_SERVER" ]]; then
+#         break
+#     fi
+#     sleep 10
+# done
+# echo "ArgoCD External IP: $ARGOCD_SERVER"
 
-# Print the admin password
-echo "Fetching the initial admin password..."
-argocd admin initial-password -n argocd
-ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd | awk 'NR==1')
+# # Print the admin password
+# echo "Fetching the initial admin password..."
+# argocd admin initial-password -n argocd
+# ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd | awk 'NR==1')
 
-# Define a retry function
-retry_command() {
-    local RETRIES=3
-    local SLEEP_TIME=10
-    local COUNTER=0
+# # Define a retry function
+# retry_command() {
+#     local RETRIES=3
+#     local SLEEP_TIME=10
+#     local COUNTER=0
 
-    until [ $COUNTER -gt $RETRIES ]; do
-        $@ && break
-        COUNTER=$((COUNTER + 1))
-        if [ $COUNTER -le $RETRIES ]; then
-            echo "Retrying ($COUNTER/$RETRIES)..."
-            sleep $SLEEP_TIME
-        fi
-    done
+#     until [ $COUNTER -gt $RETRIES ]; do
+#         $@ && break
+#         COUNTER=$((COUNTER + 1))
+#         if [ $COUNTER -le $RETRIES ]; then
+#             echo "Retrying ($COUNTER/$RETRIES)..."
+#             sleep $SLEEP_TIME
+#         fi
+#     done
 
-    if [ $COUNTER -gt $RETRIES ]; then
-        echo "Failed after $RETRIES attempts."
-        exit 1
-    fi
-}
+#     if [ $COUNTER -gt $RETRIES ]; then
+#         echo "Failed after $RETRIES attempts."
+#         exit 1
+#     fi
+# }
 
 # ArgoCD 로그인
 echo "ArgoCD 로그인 중..."
-argocd login $ARGOCD_SERVER --username admin --password $ARGOCD_PASSWORD --insecure
+argocd login www.2280.store --username admin --password $ARGOCD_PASSWORD --insecure
 
 # Secret Manager에서 시크릿 키 가져오기
 SECRET_VALUE=$(gcloud secrets versions access 1 --secret="GIT_ACCESS_TOKEN")
 
 # ArgoCD에 repo 등록
 USER_NAME=$(git config user.name)
-argocd repo add https://github.com/$USER_NAME/ING_k8s.git --username $USER_NAME --password $SECRET_VALUE --insecure 
-
+argocd repo add https://github.com/$USER_NAME/ING_k8s.git --username $USER_NAME --password $SECRET_VALUE --insecure --grpc-web
 # 다른 클러스터에 접속하기 위한 설정
 #gcloud container clusters get-credentials production --region asia-northeast3 --project protean-blend-398805
 
@@ -182,10 +132,11 @@ argocd repo add https://github.com/$USER_NAME/ING_k8s.git --username $USER_NAME 
 # ArgoCD 앱 추가
 argocd app create boutique \
   --repo https://github.com/$USER_NAME/ING_k8s.git \
-  --path GKE/cluster/overlays/app \
+  --path GKE/cluster/overlays/prod \
   --dest-namespace boutique \
   --dest-server https://kubernetes.default.svc \
   --sync-option CreateNamespace=true
-
+  --grpc-web
+  
 # argocd rollouts 대시보드 실행
 kubectl argo rollouts dashboard &

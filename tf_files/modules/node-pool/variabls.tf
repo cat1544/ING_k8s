631abeb40c3092f2891e41601442ac8254f32ab2
km00ng <mk0227_@naver.com>
2023-11-06 09:18:46 +0900

    tf_files/

---
+++ tf_files/modules/node-pool/variabls.tf
@@ -0,0 +1,29 @@
+variable "node_pool_name" {
+  type    = string
+}
+
+variable "location" {
+  type    = string
+}
+
+variable "disk_size" {
+  type    = number
+}
+
+variable "cluster_name" {}
+
+variable "label" {
+  type = map(string)
+}
+
+variable "max_pods" {
+  type = number
+}
+
+variable "min_node" {
+  type = number
+}
+
+variable "max_node" {
+  type = number
+}
---
+++ tf_files/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+  type    = string
+}
+
+variable "vpc_name" {}
+
+# variable "subnet_name" {
+#   type    = string
+# }
+
+# variable "ip_cidr_range" {
+#   type    = string
+# }
+
+# variable "region" {
+#   type    = string
+# }
+
+variable "private_ip_name" {
+  type    = string
+}
+
+variable "vpc_connection_service" {
+  type    = string
+}

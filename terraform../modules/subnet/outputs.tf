0bd987618caae3b579228ec1240743b8217ea77a
km00ng <mk0227_@naver.com>
2023-11-06 10:13:05 +0900

    Terra 폴더 제외한 테라폼 폴더 삭제

--- terraform../modules/subnet/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnetwork" {
-    value =   google_compute_subnetwork.subnet.self_link
-}
-
-output "ip_cidr_range" {
-    value =   google_compute_subnetwork.subnet.ip_cidr_range
-}
--- tf_files/modules/gcs/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "gcs_url"  {
-    description = "backend gcs url"
-    value = google_storage_bucket.backend.url
-}

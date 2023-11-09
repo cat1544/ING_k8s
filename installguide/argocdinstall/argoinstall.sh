f5e77d315d26570d75bc995ffc037503f4fbfada
cat1544 <dbstl159@naver.com>
2023-11-09 00:59:34 +0000

    db test

--- installguide/argocdinstall/argoinstall.sh
+++ installguide/argocdinstall/argoinstall.sh
@@ -136,6 +136,7 @@ argocd repo add https://github.com/$USER_NAME/ING_k8s.git --username $USER_NAME 
 
 # ArgoCD 앱 추가
 argocd app create boutique \
+  --sync-policy automated \
   --repo https://github.com/$USER_NAME/ING_k8s.git \
   --path GKE/cluster/overlays/dev \
   --dest-namespace boutique \
